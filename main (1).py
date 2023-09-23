8d05d5567eb81f7c0f6f903d0be0d19d7398d9ab
Srmano2710 <144770722+Srmano2710@users.noreply.github.com>
2023-09-23 18:57:25 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,16 +1,27 @@
-#leap year 
-"""
-year % 4 == 0 &
-year % 100 !=0 /
-year % 400 == 0
-"""
-def isleapyear(year):
-  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-     return True
-  else:
-     return False
-year = 2012
-if isleapyear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
+# Define a Student class with attributes: name, roll_number, and cgpa
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+# Define the sorting function
+def sort_students(student_list):
+    # Sort the student list based on CGPA in descending order
+    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
+    return sorted_students
+
+# Sample student objects
+students = [
+    Student("Alice", "A101", 3.8),
+    Student("Bob", "B102", 3.6),
+    Student("Charlie", "C103", 4.0),
+    Student("David", "D104", 3.9),
+]
+
+# Sorting students by CGPA in descending order
+sorted_students = sort_students(students)
+
+# Printing the sorted list
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-#
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
+# Define a function for linear search of a product
+def linear_search_product(product_list, target_product):
+    # Initialize an empty list to store indices
+    indices = []
 
-number=int(input("enter a value:"))
-res=fact_rec(number)
+    # Iterate through the list of products
+    for index, product in enumerate(product_list):
+        # Check if the current product matches the target product
+        if product == target_product:
+            # If a match is found, append the index to the list of indices
+            indices.append(index)
 
-print("the factorial of {}is{}.".format(number,res))
+    # Return the list of indices (empty list if not found)
+    return indices
 
+# Sample list of products
+products = ["Apple", "Banana", "Orange", "Apple", "Grapes", "Apple"]
+
+# Target product to search for
+target_product = "Apple"
+
+# Perform a linear search for the target product
+result = linear_search_product(products, target_product)
+
+# Display the result
+if result:
+    print(f"The product '{target_product}' was found at indices: {result}")
+else:
+    print(f"The product '{target_product}' was not found in the list.")
